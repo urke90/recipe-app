fca3c9176c55bbdcab32c89258b6256028ff0067
urke90 <urosbijelic90@gmail.com>
2020-04-12 13:48:37 +0300

    shorten recipe title

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -7,8 +7,33 @@ export const getInputValueHandler = () => domElements.searchInput.value;
 export const clearSearchInputValueHandler = () =>
   (domElements.searchInput.value = "");
 
+// removes recipes list from the UI
 export const removeRecipesHandler = () =>
   (domElements.searchResultsList.innerHTML = "");
+
+/*
+Will shorthen recipe title
+- Pasta with tomato and spinach -
+1. acc = 0, curVal = 5, => newTitle = ['Pasta']
+2. acc = 5, curVal = 4, => newTitle = ['Pasta', 'with']
+3. acc = 9, curVal = 6, => newTitle = ['Pasta', 'with', 'tomato']
+4. acc = 15, curVal = 3 // will add no more since we set the limit to 17 => newTitle = ['Pasta', 'with', 'tomato']
+*/
+const shortenRecipeTitleHandler = (title, limit = 17) => {
+  const newTitle = [];
+
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, curVal) => {
+      if (acc + curVal.length <= limit) {
+        newTitle.push(curVal);
+      }
+
+      return acc + curVal.length;
+    }, 0);
+    return `${newTitle.join(" ")}...`;
+  }
+  return title;
+};
 
 // create HTML structure for single recipe
 const renderRecipeHandler = (recipe) => {
@@ -19,7 +44,9 @@ const renderRecipeHandler = (recipe) => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${shortenRecipeTitleHandler(
+                      recipe.title
+                    )}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
